c08a238e1d6a4117255373df915c3daddbd01225
KIZUNGU Lionel <lionelkizungu@gmail.com>
2025-01-15 08:59:49 +0200

    Delete old file and add new file

--- modules.js
+++
@@ -1,9 +0,0 @@
-// Modules
-
-const names = require('./4-names');
-const SayHi = require('./5-utils');
-const data = require('./6-alternative-flavor');
-require('./7-mind-grenade');
-SayHi('Susan');
-SayHi(names.john);
-SayHi(names.peter);
