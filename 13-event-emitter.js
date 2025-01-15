c09852ca94c430dbd8fdf3752fea07611b01c035
KIZUNGU Lionel <lionelkizungu@gmail.com>
2025-01-15 12:33:22 +0200

    Update

---
+++ 13-event-emitter.js
@@ -0,0 +1,8 @@
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('response', (name, id) => {
+    console.log(`data received user: ${name} with id: ${id}`)
+})
+customEmitter.emit('response', 'john', 34)
